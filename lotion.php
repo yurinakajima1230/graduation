174ff48aeb7baacf8feff7454ec6b1e35e5dc8ab
yurinakajima1230 <168268495+yurinakajima1230@users.noreply.github.com>
2024-10-05 23:27:40 +0900

    Add files via upload

---
+++ lotion.php
@@ -0,0 +1,269 @@
+<html data-theme="light">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <title>Ski♡Skin</title>
+    <style>
+        body {
+            font-family: 'Noto Sans JP', sans-serif;
+            font-size: small;
+            }
+        .card img {
+            width: 100%;
+            height: auto;
+            max-height: 200px;
+            object-fit: contain;
+            }
+    </style>
+</head>
+<body>
+<body class="bg-gray-50">
+<header class="navbar bg-base-100 shadow-md">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a href="./lotion.php">化粧水から選ぶ</a></li>
+        <li><a href="./milk.php">乳液から選ぶ</a></li>
+        <li><a href="./trouble.php">お悩みから選ぶ</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center mx-auto">
+    <a href="./index.php" class="btn btn-ghost normal-case text-4xl font-bold text-secondary">Ski♡Skin</a>
+  </div>
+  <div class="navbar-end">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle invisible"></div>
+    </div>
+  </div>
+</header>
+    <div class="text-center my-4">
+        <p class="text-lg text-gray-600">あなたが愛用している化粧水を選んでね♡</p>
+    </div>
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=1">
+            <figure>
+                <img src="./lotion/1.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ザ・タイムR アクア</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=2">
+            <figure>
+                <img src="./lotion/2.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ハトムギ化粧水</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=3">
+            <figure>
+                <img src="./lotion/3.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">カーミングパッド</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=4">
+            <figure>
+                <img src="./lotion/4.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ドクダミ77%トナー</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=5">
+            <figure>
+                <img src="./lotion/5.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">雪肌精ブライトニング</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=6">
+            <figure>
+            <img src="./lotion/6_.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ソフィーナiP基礎化粧液</button>
+                </div>
+                </a>
+            </div>
+        </div>
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=7">
+            <figure>
+            <img src="./lotion/7.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">5番 フィルムパッド</button>
+                </div>
+                </a>
+            </div>
+        </div>        
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=8">
+            <figure>
+            <img src="./lotion/8.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">白潤プレミアム</button>
+                </div>
+                </a>
+            </div>
+        </div>       
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=9">
+            <figure>
+            <img src="./lotion/9.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ホワイトトリュフセラム</button>
+                </div>
+                </a>
+            </div>
+        </div>  
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=10">
+            <figure>
+            <img src="./lotion/10.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ネイチャーコンク</button>
+                </div>
+                </a>
+            </div>
+        </div>  
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=11">
+            <figure>
+            <img src="./lotion/11.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">スキンコンディショナー</button>
+                </div>
+                </a>
+            </div>
+        </div>  
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=12">
+            <figure>
+            <img src="./lotion/12.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">MBローション</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=13">
+            <figure>
+            <img src="./lotion/13.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ラディアント</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=14">
+            <figure>
+            <img src="./lotion/14.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">イドラクラリティ</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=15">
+            <figure>
+            <img src="./lotion/15.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">桃77％エッセンストナー</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=16">
+            <figure>
+            <img src="./lotion/16.png" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">薬用 雪肌精</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./lotionresult.php?id=17">
+            <figure>
+            <img src="./lotion/17.jfif" alt="result" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">白潤 薬用美白化粧水</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+    </div>
+</body>
+</html>
---
+++ milk.php
@@ -0,0 +1,252 @@
+<html data-theme="light">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap" rel="stylesheet">
+    <title>Ski♡Skin</title>
+    <style>
+        body {
+            font-family: 'Noto Sans JP', sans-serif;
+            font-size: small;
+        }
+        .card img {
+    width: 100%;
+    height: 150px;
+    object-fit: contain;
+}
+   
+    </style>
+</head>
+
+<body>
+<body class="bg-gray-50">
+
+<header class="navbar bg-base-100 shadow-md">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h7" />
+        </svg>
+      </div>
+      <ul
+        tabindex="0"
+        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a href="./lotion.php">化粧水から選ぶ</a></li>
+        <li><a href="./milk.php">乳液から選ぶ</a></li>
+        <li><a href="./trouble.php">お悩みから選ぶ</a></li>
+      </ul>
+    </div>
+  </div>
+  <div class="navbar-center mx-auto">
+    <a href="./index.php" class="btn btn-ghost normal-case text-4xl font-bold text-secondary">Ski♡Skin</a>
+  </div>
+
+  <div class="navbar-end">
+    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle invisible"></div>
+    </div>
+  </div>
+</header>
+
+
+
+
+
+
+    <div class="text-center my-4">
+        <p class="text-lg text-gray-600">普段あなたが愛用している乳液を選んでね♡</p>
+    </div>
+
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=1">
+            <figure>
+                <img src="./milk/1.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">白潤プレミアム美白乳液                </button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=2">
+            <figure>
+                <img src="./milk/2.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">フラルネリファイン</button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=3">
+            <figure>
+                <img src="./milk/3.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">トイロバランシング</button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=4">
+            <figure>
+                <img src="./milk/4.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ドクダミ70ローション</button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=5">
+            <figure>
+                <img src="./milk/5.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">アンフィネスホワイト
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=6">
+            <figure>
+            <img src="./milk/6.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">極潤 薬用ハリ乳液</button>
+                </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=7">
+            <figure>
+            <img src="./milk/7.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">桃77%ナイアシンミルク
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>        
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=8">
+            <figure>
+            <img src="./milk/8.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">薬用しみ対策 美白乳液
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>       
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=9">
+            <figure>
+            <img src="./milk/9.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ミノンアミノモイスト
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>  
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=10">
+            <figure>
+            <img src="./milk/10.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">白潤 薬用美白乳液
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>  
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=11">
+            <figure>
+            <img src="./milk/11.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">Norganicセラム
+                </button>
+                </div>
+                </a>
+            </div>
+        </div>  
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=12">
+            <figure>
+            <img src="./milk/12.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ドクターケイミルク
+                </button>
+                </div>
+                </a>
+            </div>
+        </div> 
+
+        <div class="card bg-base-100 shadow-xl">
+        <a href="./milkresult.php?id=13">
+            <figure>
+            <img src="./milk/13.jfif" alt="lotion" />
+            </figure>
+            <div class="card-body">
+                <div class="card-actions justify-center">
+                <button class="btn btn-outline btn-secondary text-xs">ELIXIRエマルジョン</button>
+                </div>
+                </a>
+            </div>
+        </div> 
+    </div>
+</body>
+
+</html>
